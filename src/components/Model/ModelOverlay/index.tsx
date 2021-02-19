acd7c520a9dc308bbf53c6bca897a90255860bab
Yago Passos <yp.anjos@hotmail.com>
2021-02-19 15:11:22 -0300

    Att

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -6,8 +6,13 @@ import { CarModel } from '../ModelsContext';
 interface Props{
   model: CarModel;
 }
+
+type SectionDimensions = Pick<HTMLDivElement, 'offsetTop' | 'offsetHeight'>
+
 const ModelOverlay: React.FC<Props> = ({ model, children}) => {
   const { scrollY }  = useWrapperScroll()
+
+  const getSection =
 
   return (
     <Container>
